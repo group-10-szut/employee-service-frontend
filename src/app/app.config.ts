84fdd7ee7938fe496d6fc1158d3c7b3d55f4bf87
mykhailo.bilyi <johny.johny.davis@gmail.com>
2024-01-11 11:17:27 +0100

    Integrated Guard for all routes with redirection to the keycloak login page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,9 @@ function initializeKeycloak(keycloak: KeycloakService) {
         onLoad: 'check-sso',
         silentCheckSsoRedirectUri:
           window.location.origin + '/assets/silent-check-sso.html'
-      }
+      },
+      enableBearerInterceptor: true,
+      bearerPrefix: 'Bearer',
     });
 }
 
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {Observable, of} from "rxjs";
 import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
 import {Employee} from "../Employee";
+import {KeycloakService} from "keycloak-angular";
 
 @Component({
   selector: 'app-employee-list',
@@ -12,19 +13,19 @@ import {Employee} from "../Employee";
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
-  bearer = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE3MDQ4ODMyNTcsImlhdCI6MTcwNDg3OTY1NywianRpIjoiMDEzYTNhZTctMjU1ZS00YjQzLTlmMzYtM2M0YTc5YmZiZDYzIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiI0OTEwNzEyNy0zMGI5LTQyZWQtOTg4OS0yMWZhNTlmM2ViNjkiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbInByb2R1Y3Rfb3duZXIiLCJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtc3p1dCIsInVtYV9hdXRob3JpemF0aW9uIiwidXNlciJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJ1c2VyIn0.cIfwnFGQNpCvFFRp7wVMVn6USxWREjD_jRiMpNT63PlDUiE5MERvz-5VU18Lx_cKUTjoLcdAdpfgI88qp_Lxig-FI5tT7wxiKtCe5j_OZ2i2Uq2MjTxMXsRW5_BItqd95eNK0MBrzpw5795dWV8CgDsWmMbnN63cAVDr4wpdS1x4DOsJfzXCD2-NIhpac7m_zCemDalLVVbr8yHLtZ5YXSDGp3kxkK4eoygmPFmayeubJYVORj-3eQCWcKN-1j1VSYFSMSNH-qBxX3ru-fFJJLzMjNWBmtJJrhScQD0MQFT3G14luLnSpOTLtGMCU0sacdv2WT0UXDB1rj6HGCVPBg';
   employees$: Observable<Employee[]>;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private keycloakService: KeycloakService) {
     this.employees$ = of([]);
     this.fetchData();
   }
 
-  fetchData() {
+  async fetchData() {
+    const bearerToken = await this.keycloakService.getToken();
     this.employees$ = this.http.get<Employee[]>('/backend', {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
-        .set('Authorization', `Bearer ${this.bearer}`)
+        .set('Authorization', `Bearer ${bearerToken}`)
     });
   }
 }
