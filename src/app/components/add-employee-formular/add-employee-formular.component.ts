433523bc07c0b36c6aede5f2bbbb935d29b5c12f
darleen <darleen.e1@schule.bremen.de>
2024-01-18 11:38:32 +0100

    anlegen des Formulars

--- src/app/components/add-employee-formular/add-employee-formular.component.ts
+++ src/app/components/add-employee-formular/add-employee-formular.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {MatCard} from "@angular/material/card";
 
 @Component({
   selector: 'app-add-employee-formular',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatCard],
   templateUrl: './add-employee-formular.component.html',
   styleUrl: './add-employee-formular.component.css'
 })
 export class AddEmployeeFormularComponent {
 
+  checkoutForm = this.formBuilder.group({
+    vorname: '',
+    nachname: '',
+    strasse: '',
+    plz: '',
+    stadt: '',
+    telefonnummer: '',
+    qualifikationen: ''
+  })
+
+  constructor(public formBuilder: FormBuilder) {
+
+  }
+
+  saveEmployee() {
+    // Daten an employeeService übertragen
+
+    // leert das Formular
+    this.checkoutForm.reset()
+
+    //Formular schließen
+
+    // Aufruf InfoFenster, dass MA gespeichert wurde
+  }
 }
