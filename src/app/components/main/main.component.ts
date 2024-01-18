c270a4ef25e6341e96b5ae253ab574a377a49777
darleen <darleen.e1@schule.bremen.de>
2024-01-18 11:41:17 +0100

    Import wegen Änderung in der Ordnerstruktur geändert

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {EmployeeListComponent} from '../employee-list/employee-list.component';
+import {AddEmployeeFormularComponent} from "../add-employee-formular/add-employee-formular.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [EmployeeListComponent],
+  imports: [EmployeeListComponent, AddEmployeeFormularComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
