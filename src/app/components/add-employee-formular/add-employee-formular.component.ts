8262a0ff7dcf0956db5d1608b2ad665fbb23acb6
darleen <darleen.e1@schule.bremen.de>
2024-01-21 12:24:23 +0100

    Employee Form angepasst

--- src/app/components/add-employee-formular/add-employee-formular.component.ts
+++ src/app/components/add-employee-formular/add-employee-formular.component.ts
@@ -1,16 +1,22 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatCard} from "@angular/material/card";
+import {QualificationService} from "../../services/qualification.service";
+import {SkillGet} from "../../model/skill-get";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-add-employee-formular',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatCard],
+  imports: [CommonModule, ReactiveFormsModule, MatCard, FormsModule],
   templateUrl: './add-employee-formular.component.html',
   styleUrl: './add-employee-formular.component.css'
 })
 export class AddEmployeeFormularComponent {
+  constructor(public formBuilder: FormBuilder, public qualificationService: QualificationService) {
+    this.skillsString.push("Java", "Angular", "Cobol");
+  }
 
   checkoutForm = this.formBuilder.group({
     vorname: '',
@@ -22,18 +28,29 @@ export class AddEmployeeFormularComponent {
     qualifikationen: ''
   })
 
-  constructor(public formBuilder: FormBuilder) {
+  skillsString: string[] = [];
+  selectedSkills: string[] = [];
 
-  }
+  skills: SkillGet[] = [];
+  skill: SkillGet | undefined;
+  skillFromApi: string | undefined;
 
   saveEmployee() {
     // Daten an employeeService übertragen
 
     // leert das Formular
-    this.checkoutForm.reset()
+    this.checkoutForm.reset();
 
     //Formular schließen
 
     // Aufruf InfoFenster, dass MA gespeichert wurde
+  }
+
+  getSkillListForDropdown() {
+    console.log(this.qualificationService.getListOfAllQualifications());
+  }
+
+  closeForm() {
+
   }
 }
