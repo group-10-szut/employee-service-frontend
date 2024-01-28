29eb294affe6b086d248366137922687886aaae2
darleen <darleen.e1@schule.bremen.de>
2024-01-28 11:28:56 +0100

    injiziert ShareService

--- src/app/components/buttons/add-qualification-button/add-qualification-button.component.ts
+++ src/app/components/buttons/add-qualification-button/add-qualification-button.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
+import {ShareService} from "../../../services/share.service";
 
 @Component({
   selector: 'app-add-qualification-button',
@@ -14,6 +15,14 @@ import { ButtonComponent } from '../button/button.component';
   `
 })
 export class AddQualificationButtonComponent {
+
+  isFormularShowed: boolean = false;
+
+  constructor(public shareService: ShareService) {
+    this.shareService.isQualificationFormShowed.subscribe(
+      currentStatus => this.isFormularShowed = currentStatus);
+  }
+
   onAddQualificationClick() {
     // Specific logic for adding qualifications
     console.log('Add Qualification Logic');
