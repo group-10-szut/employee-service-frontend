cef57d3251cdac97601c2bbee1a96ffccdf19591
darleen <darleen.e1@schule.bremen.de>
2024-01-28 11:30:49 +0100

    passt den ShareService an

--- src/app/services/share.service.ts
+++ src/app/services/share.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,13 @@ export class ShareService {
   constructor() {
   }
 
-  openEmployeeForm() {
+  // mit BehaviorSubject und Observable ist sichergestellt, dass jede Komponente,
+  // die diesen Service nutzt, den aktuellen Wert erhält
+  private qualificationFormStatus = new BehaviorSubject<boolean>(false);
+  isQualificationFormShowed = this.qualificationFormStatus.asObservable();
 
-  };
+  // ändert den Wert des Observables
+  changeShowQualificationForm(show: boolean) {
+    this.qualificationFormStatus.next(show);
+  }
 }
